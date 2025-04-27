989e8a2077369bf94306760820cea6ca25a1bfa1
kwnP <kwangwon0424@naver.com>
2025-04-28 00:12:47 +0900

    2주차 todo 수정

--- week02/박광원/todo/src/App.jsx
+++ week02/박광원/todo/src/App.jsx
@@ -2,52 +2,71 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-    const [input, setInput] = useState("");
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    const handleAddTodo = () => {
-        if (input.trim() === "") return;
-        setTodos([...todos, input]);
-        setInput("");
-    };
-    return (
-        <>
-            <TodoInput
-                input = {input}
-                setInput = {setInput}
-                handleAddTodo= {handleAddTodo}
-            />
-            <TodoList
-                todos = {todos}
-            />
-        </>
-    )
+  const deleteTodo = (id) => {
+    setTodos(todos => todos.filter((todo) => todo.id !== id));
+  }
+  
+  return (
+    <>
+      <TodoInput
+        todos = {todos}
+        setTodos = {setTodos}
+      />
+      <ul>
+        {todos.map((todo) => (
+          <TodoList
+            key={todo.id}
+            todo = {todo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ul>
+    </>
+  )
 }
 
-const TodoInput = ({input, setInput, handleAddTodo}) => {
-    return(
-        <div>
-            <input
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                placeholder="할 일을 입력하세요"
-            />
-            <button onClick={handleAddTodo} >추가</button>
-        </div>
-    )
+const TodoInput = ({todos, setTodos}) => {
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (input.trim() === "") return;
+    const newTodo = {
+      id: Date.now(),
+      text: input
+    }
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
+
+  return(
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="할 일을 입력하세요"
+      />
+      <button onClick={addTodo}>추가</button>
+    </div>
+  )
 }
 
-const TodoList = ({todos}) => {
-    return (
-        <ul>
-            {todos.map((todo, index) => (
-                <li key={index}>
-                    {todo}
-                </li>
-            ))}
-        </ul>
-    )
+const TodoList = ({todo, deleteTodo}) => {
+  const [check, setCheck] = useState(false);
+  const isChecked = () => {
+    setCheck(!check);
+  }
+  console.log(todo)
+
+  return (
+    <li>
+      <input type="checkbox" value={check} onChange={isChecked}/>
+      {check === true ? <del>{todo.text}</del> : todo.text}
+      <button onClick={() => deleteTodo(todo.id)}>삭제</button>
+    </li>
+  )
 }
 
 export default App
