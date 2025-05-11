0213951f2a51b01be2ec85da4d7872c09fed2f15
kwnP <kwangwon0424@naver.com>
2025-05-11 16:11:39 +0900

    6주차 업로드

---
+++ week06/박광원/todo/src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+export default function TodoList({todo, deleteTodo, checkTodo}) {
+  const isChecked = () => {
+    checkTodo(todo.id);
+  }
+  
+  return (
+    <li className={todo.checked === true? "rounded-xl px-4 py-4 flex justify-between items-center mb-4 bg-red-100" : "rounded-xl px-4 py-4 flex justify-between items-center mb-4 bg-gray-100"}>
+      <input type="checkbox" onChange={isChecked} checked={todo.checked}/>
+      {todo.checked === true ? <del>{todo.text}</del> : todo.text}
+      <button className={todo.checked === true? "rounded-lg py-2 px-4 bg-red-300 hover:bg-red-500 hover:text-white" : "rounded-lg py-2 px-4 bg-gray-300 hover:bg-gray-500 hover:text-white"} onClick={() => deleteTodo(todo.id)}>삭제</button>
+    </li>
+  )
+}
